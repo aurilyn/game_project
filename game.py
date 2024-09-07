c8968c61fba7106ec65d94bf7949bbaa6ccbbb40
aurilyn <caobenny122@gmail.com>
2024-09-07 08:19:43 -0700

    player and mobs can take damage now

--- game.py
+++ game.py
@@ -1,23 +1,94 @@
-class Options:
-    def prompt(self):
-        chamber = []
-        limit = 3
-        print(f"You have a limit of {limit} attacks you can make. Please select the order in which you want to send your attacks.")
-        for i in range(limit):
-            a = input(f"Please select attack {i+1}: ")
-            chamber.append(a)
-        # print(chamber)
-        return chamber
+import random
+import numpy as np
 
-    def view_deck(self, deck):
-        print(deck)
+# class Options:
+#     def __init__(self):
+#         self.player_health = 15
+
+#     def prompt(self):
+#         chamber = []
+#         limit = 3
+#         print(f"You have a limit of {limit} attacks you can make. Please select the order in which you want to send your attacks.")
+#         for i in range(limit):
+#             a = input(f"Please select attack {i+1}: ")
+#             chamber.append(a)
+#         # print(chamber)
+#         return chamber
+    
+#     def get_player_health(self):
+#         return self.player_health
+    
+#     def update_player_health(self, new_health):
+#         self.player_health = new_health
+
+#     def view_deck(self, deck):
+#         print(deck)
 
 class Game_Start:
-    def deck_init(self):
-        deck = ["Rock", "Paper", "Scissors"]
-        return deck
+    def __init__(self):
+        self.player = Player()
+        self.mob = Mobs()
+
+class Player:
+    def __init__(self) -> None:
+        self.player_health = 15
+        self.player_armour = 0
+        self.player_gold = 10
+        self.player_deck = ["Rock", "Paper", "Scissors"]
     
-    def gold_init(self):
-        gold = 15
-        return gold
+    def get_player_health(self):
+        return self.player_health
     
+    def update_player_health(self, new_health):
+        self.player_health = new_health
+        
+class Mobs:
+    def __init__(self) -> None:
+        self.mob_health = 15
+        self.mob_armour = 0
+        self.attacks = ["Rock", "Paper", "Scissors"]
+
+    def mob_attack(self):
+        mob_attack = np.random.choice(self.attacks, 3)
+        return mob_attack
+
+    def update_mob_health(self, new_health):
+        self.mob_health = new_health
+
+class Combat:
+    def __init__(self):
+        self.winning_conditions = {
+            "Rock": "Scissors",
+            "Paper": "Rock",
+            "Scissors": "Paper"
+        }
+
+    def damage(self, player_attack, mob_attack):
+        if player_attack == mob_attack:
+            return 0
+        elif self.winning_conditions[player_attack] == mob_attack:
+            return 1
+        else:
+            return -1
+            
+    def combat(self, player_attacks, mob_attacks):
+        mob_damage = 0
+        player_damage = 0
+
+        for player_attack, mob_attack in zip(mob_attacks, player_attacks):
+            result = self.damage(player_attack, mob_attack)
+            if result == 1:
+                player_damage += 1
+            elif result == -1:
+                mob_damage += 1
+            else:
+                pass
+        return mob_damage, player_damage
+    
+    def damage_calc(self, player_damage, mob_damage):
+        mobs = Mobs()
+        player = Player()
+
+        player.player_health -= mob_damage
+        mobs.mob_health -= player_damage    
+        return player.player_health, mobs.mob_health
--- test.py
+++ test.py
@@ -1,8 +1,32 @@
 from game import *
+import time
 
-game_init = Game_Start()
-opt = Options()
-deck = game_init.deck_init()
-# print(game_init.deck_init(), game_init.gold_init())
+game = Game_Start()
+player = Player()
+mob = Mobs()
+combat = Combat()
 
-opt.prompt()
+mob_attacks = mob.mob_attack()
+print(mob_attacks)
+player_attacks = []
+player_attack_limit = 3
+#allow the player to select their attacks
+time.sleep(5)
+while len(player_attacks) < player_attack_limit:
+    player_input = input("Please select which attack you want to use: ")
+    #verify if the input is the right type
+    if player_input not in ["Rock", "Paper", "Scissors"]:
+        player_input = input("Please try again.")
+    else:
+        player_attacks.append(player_input)
+        print(f"You have {player_attack_limit - len(player_attacks)} attacks left")
+
+#after player selects their attacks, go to combat
+player_damage, mob_damage = combat.combat(player_attacks, mob_attacks)
+
+new_player_health, new_mob_health = combat.damage_calc(player_damage, mob_damage)
+player.update_player_health(new_player_health)
+mob.update_mob_health(new_mob_health)
+
+print(f"Player Health: {player.get_player_health()}")
+print(f"Mob Health: {mob.mob_health}")
